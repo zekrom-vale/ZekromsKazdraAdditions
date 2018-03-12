450730e5038609e490f3644655946673a56ad608
zekrom-vale <sgraven66@gmail.com>
2018-03-11 22:03:20 -0500

    2space->1tab

--- ZekromsKazdraAdditions/ZekromsKazdraAdditions/items/active/weapons/ranged/abilities/electricthrower/electricthrower.lua
+++ ZekromsKazdraAdditions/ZekromsKazdraAdditions/items/active/weapons/ranged/abilities/electricthrower/electricthrower.lua
@@ -3,34 +3,34 @@ require "/items/active/weapons/ranged/gunfire.lua"
 ElectricthrowerAttack = GunFire:new()
 
 function ElectricthrowerAttack:init()
-  GunFire.init(self)
+	GunFire.init(self)
 
-  self.active = false
+	self.active = false
 end
 
 function ElectricthrowerAttack:update(dt, fireMode, shiftHeld)
-  GunFire.update(self, dt, fireMode, shiftHeld)
+	GunFire.update(self, dt, fireMode, shiftHeld)
 
-  if self.weapon.currentAbility == self then
-    if not self.active then self:activate() end
-  elseif self.active then
-    self:deactivate()
-  end
+	if self.weapon.currentAbility == self then
+		if not self.active then self:activate() end
+	elseif self.active then
+		self:deactivate()
+	end
 end
 
 function ElectricthrowerAttack:muzzleFlash()
-  --disable normal muzzle flash
+	--disable normal muzzle flash
 end
 
 function ElectricthrowerAttack:activate()
-  self.active = true
-  animator.playSound("fireStart")
-  animator.playSound("fireLoop", -1)
+	self.active = true
+	animator.playSound("fireStart")
+	animator.playSound("fireLoop", -1)
 end
 
 function ElectricthrowerAttack:deactivate()
-  self.active = false
-  animator.stopAllSounds("fireStart")
-  animator.stopAllSounds("fireLoop")
-  animator.playSound("fireEnd")
+	self.active = false
+	animator.stopAllSounds("fireStart")
+	animator.stopAllSounds("fireLoop")
+	animator.playSound("fireEnd")
 end
--- ZekromsKazdraAdditions/ZekromsKazdraAdditions/items/active/weapons/ranged/abilities/icethrower/icethrower.lua
+++ ZekromsKazdraAdditions/ZekromsKazdraAdditions/items/active/weapons/ranged/abilities/icethrower/icethrower.lua
@@ -3,34 +3,34 @@ require "/items/active/weapons/ranged/gunfire.lua"
 IcethrowerAttack = GunFire:new()
 
 function IcethrowerAttack:init()
-  GunFire.init(self)
+	GunFire.init(self)
 
-  self.active = false
+	self.active = false
 end
 
 function IcethrowerAttack:update(dt, fireMode, shiftHeld)
-  GunFire.update(self, dt, fireMode, shiftHeld)
+	GunFire.update(self, dt, fireMode, shiftHeld)
 
-  if self.weapon.currentAbility == self then
-    if not self.active then self:activate() end
-  elseif self.active then
-    self:deactivate()
-  end
+	if self.weapon.currentAbility == self then
+		if not self.active then self:activate() end
+	elseif self.active then
+		self:deactivate()
+	end
 end
 
 function IcethrowerAttack:muzzleFlash()
-  --disable normal muzzle flash
+	--disable normal muzzle flash
 end
 
 function IcethrowerAttack:activate()
-  self.active = true
-  animator.playSound("fireStart")
-  animator.playSound("fireLoop", -1)
+	self.active = true
+	animator.playSound("fireStart")
+	animator.playSound("fireLoop", -1)
 end
 
 function IcethrowerAttack:deactivate()
-  self.active = false
-  animator.stopAllSounds("fireStart")
-  animator.stopAllSounds("fireLoop")
-  animator.playSound("fireEnd")
+	self.active = false
+	animator.stopAllSounds("fireStart")
+	animator.stopAllSounds("fireLoop")
+	animator.playSound("fireEnd")
 end
